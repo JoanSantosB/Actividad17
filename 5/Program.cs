ae9f9ac667a4787561ced35833394c1066e923d5
ander <ander@cjoanbatz@gmail.com>
2026-04-12 21:23:10 -0600

    Terminé el ejercicio 5

---
+++ 5/Program.cs
@@ -0,0 +1,22 @@
+﻿bool correcto = false;
+do
+{
+    Console.Write("Ingrese un número: ");
+    if (int.TryParse(Console.ReadLine(), out int numero))
+    {
+
+        correcto = true;
+        if(numero%2==0)
+        {
+            Console.WriteLine($"El número {numero} es par.");
+        }
+        else
+        {
+            Console.WriteLine($"El número {numero} es impar.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Entrada no válida. Por favor, ingrese un número entero.");
+    }
+} while (!correcto);
