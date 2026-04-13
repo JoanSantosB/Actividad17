c6c36923e4ebf443c2e4fa923821f164d1e63a0f
ander <ander@CUQUEBATZ>
2026-04-12 20:57:29 -0600

    Agregar archivos de proyecto.

---
+++ 1/Program.cs
@@ -0,0 +1,21 @@
+﻿bool correcto = false;
+do
+{
+    Console.Write("Ingrese se una edad:");
+    if(int.TryParse(Console.ReadLine(), out int edad))
+    {
+        if(edad > 0 && edad <= 120)
+        {
+            Console.WriteLine($"La edad ingresada es: {edad}");
+            correcto = true;
+        }
+        else
+        {
+            Console.WriteLine("La edad debe estar entre 0 y 120.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Entrada no válida. Por favor, ingrese un número entero.");
+    }
+} while(!correcto);
