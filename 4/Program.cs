0727d554788ba7def099309305df4d6ad9d62c8e
ander <ander@cjoanbatz@gmail.com>
2026-04-12 21:18:55 -0600

    Terminé el ejercicio 4

---
+++ 4/Program.cs
@@ -0,0 +1,21 @@
+﻿bool correcto = false;
+do
+{
+    Console.Write("Ingrese su año de nacimiento: ");
+    if (int.TryParse(Console.ReadLine(), out int year))
+    {
+        if (year >= 1900 && year <= 2026)
+        {
+            Console.WriteLine($"Año correcto: {year}");
+            correcto = true;
+        }
+        else
+        {
+            Console.WriteLine("El año no esta en el rango de 1900 a 2026");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Entrada no válida. Por favor, ingrese un número entero.");
+    }
+} while (!correcto);
