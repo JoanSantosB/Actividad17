db397735e5a09238cf47fd480510ddfd9b7a0cf8
ander <ander@cjoanbatz@gmail.com>
2026-04-13 09:15:44 -0600

    Terminé el ejercicio 9

---
+++ 9/Program.cs
@@ -0,0 +1,28 @@
+﻿bool correcto = false;
+do
+{
+    Console.Write("Ingrese una nota: ");
+    if (int.TryParse(Console.ReadLine(), out int nota))
+    {
+        if (nota >= 0 && nota <= 100)
+        {
+            correcto = true;
+            if (nota < 60)
+            {
+                Console.WriteLine($"Nota {nota}--Reprobada");
+            }
+            else
+            {
+                Console.WriteLine($"Nota {nota}--Aprobada");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Error debe la nota debe estar en un rango de 0 a 100.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Entrada no válida. Por favor, ingrese una nota válida.");
+    }
+} while (!correcto);
