3cba199a629308c331ee990bd8ba6df26aeffe8b
ander <ander@cjoanbatz@gmail.com>
2026-04-12 21:10:18 -0600

    Terminé el ejercicio 3

---
+++ 3/Program.cs
@@ -0,0 +1,21 @@
+﻿bool correcto = false;
+do
+{
+    Console.Write("Ingrese una cantidad de estudiantes: ");
+    if (int.TryParse(Console.ReadLine(), out int cantidad))
+    {
+        if (cantidad > 0)
+        {
+            Console.WriteLine($"Cantidad válida: {cantidad}");
+            correcto = true;
+        }
+        else
+        {
+            Console.WriteLine("No se puede realizar esta opceración, debe ser una cantidad mayor que 0");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Entrada no válida. Por favor, ingrese un número entero.");
+    }
+}while(!correcto);
