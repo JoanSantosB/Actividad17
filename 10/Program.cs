ab8afa7eb601c68a6f2eb41cab5b41db7699cac6
ander <ander@cjoanbatz@gmail.com>
2026-04-13 09:23:50 -0600

    Terminé el ejercicio 10

---
+++ 10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+
+bool correcto = false;
+do
+{
+    Console.Write("Ingrese la cantidad de productos a comprar: ");
+    if (int.TryParse(Console.ReadLine(), out int cantidad)&&cantidad>0)
+    {
+        correcto = true;
+        for(int i = 1; i <= cantidad; i++)
+        {
+            Console.Write($"Ingrese el producto {i}: ");
+            if(double.TryParse(Console.ReadLine(),out double precio) && precio >= 0)
+            {
+                Console.WriteLine($"Producto: {i}--Precio: {precio:F2}");
+            }
+            else
+            {
+                Console.WriteLine("Error ingreseo un dato inválido");
+                i--;
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("Entrada no válida. Por favor, ingrese una cantidad válida.");
+    }
+} while (!correcto);
