8c0b1e71a55ec32bc829b0afefd1fb122ce4d5a5
ander <ander@cjoanbatz@gmail.com>
2026-04-13 08:55:52 -0600

    Terminé el ejercicio 7

---
+++ 7/Program.cs
@@ -0,0 +1,21 @@
+﻿bool correcto = false;
+do
+{
+    Console.Write("Ingrese un salario: ");
+    if(double.TryParse(Console.ReadLine(),out double salario))
+    {
+        if (salario >= 0)
+        {
+            Console.WriteLine($"Salario ingresado: {salario:F2}");
+            correcto = true;
+        }
+        else
+        {
+            Console.WriteLine("Error el salario debe ser un número positivo");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Entrada no válida. Por favor, ingrese un salario válido.");
+    }
+}while(!correcto);
