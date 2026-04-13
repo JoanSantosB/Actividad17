ca921cd9256279fecb4401cec9b84c0487116fbe
ander <ander@cjoanbatz@gmail.com>
2026-04-12 21:29:39 -0600

    Terminpe el ejercicio 6

---
+++ 6/Program.cs
@@ -0,0 +1,14 @@
+﻿bool correcto = false;
+do
+{
+    Console.Write("Ingrese una fecha: ");
+    if (DateTime.TryParse(Console.ReadLine(), out DateTime fecha))
+    {
+        Console.WriteLine($"Fecha ingresada: {fecha.ToShortDateString()}");
+        correcto = true;
+    }
+    else
+    {
+        Console.WriteLine("Entrada no válida. Por favor, ingrese una fecha válida.");
+    }
+} while (!correcto);
